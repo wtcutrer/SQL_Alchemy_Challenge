cb07ff4f2134bad2c65905ca419bad48b3675590
Tyler Cutrer <wtcutrer@gmail.com>
2021-09-09 18:14:27 -0500

    Final changes

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
+#Create engine and start session
 engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread': False})
 
-
+#Create automap
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 Base.classes.keys()
@@ -24,43 +25,48 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return (f"Surfs Up! Welcom to my SQLAlchemy Challenge API!<br/>"
+    return (f"Welcome<br/>"
             f"----------------------------------------------------<br/>"
-            f"Here are the Available Routes:<br/>"
+            f"Available Routes:<br/>"
             f"----------------------------------------------------<br/>"
-            f"/api/v1.0/precipitaton -- This Route gives Data on Preceipitation data<br/>"
+            f"/api/v1.0/precipitaton -- Preceipitation data<br/>"
             f"----------------------------------------------------<br/>"
-            f"/api/v1.0/stations ------ This Route gives Data on Weather observation stations<br/>"
+            f"/api/v1.0/stations ------ Weather observation stations<br/>"
             f"----------------------------------------------------<br/>"
-            f"/api/v1.0/temperature --- This Route gives Data on Temperature Observation Data (TOBS) with the Most Active Station<br/>"
+            f"/api/v1.0/temperature --- Temperature data<br/>"
             f"----------------------------------------------------<br/>"
-            f"/api/v1.0/start---------- Datesearch (yyyy-mm-dd)--- This Route lists the minimum temperature, the average temperature, and the max temperature for date given.<br/>"
+            f"/api/v1.0/start---------- Datesearch (yyyy-mm-dd)--- Lists the minimum temperature, the average temperature, and the max temperature for date given.<br/>"
             f"----------------------------------------------------<br/>"
-            f"/api/v1.0/start/end-------Datesearch (yyyy-mm-dd/yyyy-mm-dd)-- This Route lists the minimum temperature, the average temperature, and the max temperature for date range given.<br/>")
+            f"/api/v1.0/start/end-------Datesearch (yyyy-mm-dd/yyyy-mm-dd)-- Lists the minimum temperature, the average temperature, and the max temperature for date range given.<br/>")
 
-@app.route("/api/v1.0/precipitation")
+@app.route("/api/v1.0/precipitaton")
 def precipitation():
-    #Create a queries that filters for only the last year of Precipitation Data
+      #Create a queries that filters for only the last year of Precipitation Data
     results = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.date >= "2016-08-23").all() 
+        filter(Measurement.date >= "2016-08-23").all()
     results_json = [results]
     return jsonify(results_json)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    #Return a list of the stations
+     #Return a list of the stations
     results = session.query(Station.name).all()
     stations_json = list(np.ravel(results))
     return jsonify(stations_json) 
                    
-@app.route("/api/v1.0/tobs")
-def temp_obs():                   
-   #Return TOBS from last year and filter them down by msot active station
-    results = session.query(Station.name, Measurement.date, Measurement.tobs).\
-        filter(Measurement.date >= "2016-08-23").filter(Measurement.station == 'USC00519281').all()
-    obs_json = list(np.ravel(results))
-    return jsonify(obs_json)      
-                                
+@app.route("/api/v1.0/temperature")   
+    #Return TOBS from last year and filter them down by msot active station         
+def temp_monthly():
+    # Calculate the year
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    #create query
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)         
+                   
 @app.route("/api/v1.0/<start>")
 def start_date(start):
     results = session.query(Measurement.date, func.avg(Measurement.tobs), func.max(Measurement.tobs),func.min(Measurement.tobs)).\
@@ -73,7 +79,7 @@ def start_end(start, end):
     results = session.query(func.avg(Measurement.tobs), func.max(Measurement.tobs), func.min(Measurement.tobs)).\
         filter(Measurement.date >= start, Measurement.date <= end).all()
     end_json = list(np.ravel(results))
-    return jsonify(end_json)        
+    return jsonify(end_json)
         
 if __name__ == '__main__':
     app.run(debug=True) 
